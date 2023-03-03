d9897353c0fe4b9c3552b0f2b8c934466df7e06a
SofiaBCIT <sofia.sirianni@outlook.comm>
2023-03-02 16:01:16 -0800

    pls deploy

--- pages/channels/[channelId].js
+++ pages/channels/[channelId].js
@@ -2,7 +2,7 @@ import { getAllMessages, } from "@/database";
 import { useState } from "react";
 import Head from 'next/head'
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import styles from '../../styles/Home.module.css'
 import axios from "axios";
 
